ab779c8523791df6f457bb0a49569d058980d61f
Robert Brands <mail@robert-brands.com>
2022-08-25 16:44:07 +0000

    Version

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using BlazorApp.Client.Utils;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<BackendApiRepository>();
 
 await builder.Build().RunAsync();
