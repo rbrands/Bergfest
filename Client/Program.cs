6658b1853d0fad09bb2aa8f31b0d4db0dd5fd56d
Robert Brands <mail@robert-brands.com>
2022-08-26 05:24:14 +0000

    Cleanup

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<BackendApiRepository>();
+builder.Services.AddSingleton<AppState>();
 
 await builder.Build().RunAsync();
