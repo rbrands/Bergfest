8ed024ee19c81b595a3bbd284f57cd09e9e841ac
Robert Brands (RiwaAdmin) <rbadmin@riwa4.de>
2022-09-09 14:55:05 +0200

    Webhook

--- Bergfest-Webhook/StravaWebhook.cs
+++ Bergfest-Webhook/StravaWebhook.cs
@@ -11,6 +11,8 @@ using Microsoft.AspNetCore.Routing;
 using BlazorApp.Shared;
 using Bergfest_Webhook.Utils;
 using System.Text.Json;
+using System.Dynamic;
+using Microsoft.Extensions.Configuration;
 
 namespace Bergfest_Webhook
 {
@@ -18,41 +20,77 @@ namespace Bergfest_Webhook
     {
         private readonly ILogger _logger;
         private StravaRepository _stravaRepository;
+        private IConfiguration _config;
 
         public StravaWebhook(ILogger<StravaWebhook> logger,
+                             IConfiguration config,
                              StravaRepository stravaRepository
                             )
         {
             _logger = logger;
+            _config = config;
             _stravaRepository = stravaRepository;
         }   
-        [FunctionName("ValidateStravaWebhook")]
-        public ObjectResult RunValidation(
-                [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ValidateStravaWebhook")] HttpRequest req
+        [FunctionName("StravaWebhook")]
+        public async Task<IActionResult> Run(
+                [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "StravaWebhook")] HttpRequest req
             )
         {
             try
             {
-                string verifyToken = req.Query["hub.verify_token"];
-                string hubChallenge = req.Query["hub.challenge"];
-                _logger.LogInformation($"Strava Webhook validation request verifyToken={verifyToken} hubChallenge={hubChallenge}");
-                if (String.Compare(verifyToken, Constants.STRAVA_WEBHOOK_VERIFY_TOKEN) != 0)
-                {
-                    throw new Exception("Verify token doesn't match.");
+                if (req.Method == "GET")
+                { 
+                    string responseSerialized = ValidateSubscrption(req);
+                    return new OkObjectResult(responseSerialized);
                 }
-                StravaValidationResponse response = new StravaValidationResponse();
-                response.HubChallenge = hubChallenge;
-                string responseSerialized = JsonSerializer.Serialize(response);
-                _logger.LogInformation(responseSerialized);
-                return new OkObjectResult(responseSerialized);
+                else if (req.Method == "POST")
+                {
+                    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                    _logger.LogInformation($"StravaWebhook {requestBody}");
+                    dynamic postRequest = JsonSerializer.Deserialize<ExpandoObject>(requestBody);
+                    HandleWebhookPost(postRequest);
+                    return new OkResult();
+                }
+                else
+                {
+                    throw new Exception($"StravaWebhook called with invalid method {req.Method}");
+                }
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "AuthorizeBergFestOnStrava failed.");
+                _logger.LogError(ex, "StravaWebhook failed.");
                 return new BadRequestObjectResult(ex.Message);
             }
-           
         }
-         
+
+        internal void HandleWebhookPost(dynamic postRequest)
+        {
+            string subscriptionId = _config["STRAVA_SUBSCRIPTION_ID"];
+            if (String.IsNullOrEmpty(subscriptionId))
+            {
+                throw new Exception("STRAVA_SUBSCRIPTION_ID not configured.");
+            }
+            _logger.LogInformation($"Subscription-Id expected: {subscriptionId}");
+            if (subscriptionId != postRequest.subscription_id.ToString())
+            {
+                throw new Exception("Wrong subscription id.");
+            }
+            _logger.LogInformation($"Webhook for subscription_id {postRequest.subscription_id} object_id {postRequest.object_id}");
+        }
+        internal string ValidateSubscrption(HttpRequest req)
+        {
+            string verifyToken = req.Query["hub.verify_token"];
+            string hubChallenge = req.Query["hub.challenge"];
+            _logger.LogInformation($"Strava Webhook validation request verifyToken={verifyToken} hubChallenge={hubChallenge}");
+            if (String.Compare(verifyToken, Constants.STRAVA_WEBHOOK_VERIFY_TOKEN) != 0)
+            {
+                throw new Exception("Verify token doesn't match.");
+            }
+            StravaValidationResponse response = new StravaValidationResponse();
+            response.HubChallenge = hubChallenge;
+            string responseSerialized = JsonSerializer.Serialize(response);
+            _logger.LogInformation(responseSerialized);
+            return responseSerialized;
+        }
     }
 }
