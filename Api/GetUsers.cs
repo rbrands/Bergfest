6eb2d071e4b58dabec16fa16f13be85185eb2388
Robert Brands (RiwaAdmin) <rbadmin@riwa4.de>
2022-09-20 07:26:25 +0200

    List of users

---
+++ Api/GetUsers.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using BlazorApp.Shared;
+using System.Web.Http;
+using BlazorApp.Api.Repositories;
+using BlazorApp.Api.Utils;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace BlazorApp.Api
+{
+    public class GetUsers
+    {
+        private readonly ILogger _logger;
+        private readonly IConfiguration _config;
+        private StravaRepository _cosmosRepository;
+
+        public GetUsers(ILogger<GetUsers> logger,
+                        IConfiguration config,
+                        StravaRepository cosmosRepository
+        )
+        {
+            _logger = logger;
+            _config = config;
+            _cosmosRepository = cosmosRepository;
+        }
+
+        [FunctionName(nameof(GetUsers))]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetUsers")] HttpRequest req)
+        {
+            try
+            {
+                IEnumerable<StravaAccess> userInfos = await _cosmosRepository.GetItems();
+
+                return new OkObjectResult(userInfos);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestErrorMessageResult(ex.Message);
+            }
+        }
+    }
+}
--- Client/Utils/BackendApiRepository.cs
+++ Client/Utils/BackendApiRepository.cs
@@ -114,6 +114,11 @@ namespace BlazorApp.Client.Utils
                 throw new Exception(error?.Message);
             }
         }
+        public async Task<IEnumerable<StravaAccess>?> GetUsers()
+        {
+            this.PrepareHttpClient();
+            return await _http.GetFromJsonAsync<IEnumerable<StravaAccess>?>($"/api/GetUsers");
+        }
         public async Task<IEnumerable<StravaSegmentWithEfforts>> GetSegmentsWithEfforts()
         {
             this.PrepareHttpClient();
